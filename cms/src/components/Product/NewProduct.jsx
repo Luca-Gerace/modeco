e6b69a1b1b0f2ec66597bdd2053ed7df42f60109
Luca-Gerace <geraceluca91@gmail.com>
2024-08-27 15:08:11 +0200

    CMS: add product modal

--- cms/src/components/Product/NewProduct.jsx
+++ cms/src/components/Product/NewProduct.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react';
-import { Dialog, DialogHeader, DialogBody, DialogFooter, Button, Input, Select, Option } from '@material-tailwind/react';
+import { Dialog, DialogHeader, DialogBody, Button, Input, Select, Option } from '@material-tailwind/react';
 import { createProduct } from '../../services/api';
 import Alert from '../../../../frontend/src/components/Alert';
+import { IconButton, Typography } from '@mui/material';
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
 function NewProductModal({ open, handleOpen, setAllProducts }) {
-
-    // Hook - taglie prodotto
+    const [step, setStep] = useState(1); // Step corrente
     const [sizes, setSizes] = useState([]);
     const [newSize, setNewSize] = useState('');
-
-    // Hook - immagine del prodotto
     const [productImage, setProductImage] = useState(null);
-
-    // Hook - prodotto nuovo
     const [newProduct, setNewProduct] = useState({
         brand: "",
         name: "",
@@ -21,12 +18,10 @@ function NewProductModal({ open, handleOpen, setAllProducts }) {
         description: "",
         quantity: 0,
         color: "",
+        ingredients: "",
+        nutritionFacts: ""
     });
-
-    // Hook - category
     const [category, setCategory] = useState("clothes");
-
-    // Hook - alert
     const [alert, setAlert] = useState(null);
 
     const handleAddSize = () => {
@@ -45,17 +40,15 @@ function NewProductModal({ open, handleOpen, setAllProducts }) {
         setNewProduct({ ...newProduct, [name]: value });
     };
 
-    // Product file handler
     const handleFileChange = (e) => {
         setProductImage(e.target.files[0]);
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+
         try {
             const formData = new FormData();
-            // Conversion to Number
             formData.append('price', Number(newProduct.price));
             formData.append('quantity', Number(newProduct.quantity));
 
@@ -74,13 +67,13 @@ function NewProductModal({ open, handleOpen, setAllProducts }) {
                     formData.append('size[]', size);
                 });
             }
-    
+
             const newProductResponse = await createProduct(formData);
             setAllProducts(prevProducts => [...prevProducts, newProductResponse.data]);
 
             handleOpen();
             setAlert({ message: 'Product created successfully!', type: 'success' });
-    
+
         } catch (error) {
             console.error("Error creating the product:", error.response.data);
             setAlert({ message: 'Product creation error. Retry.', type: 'error' });
@@ -92,125 +85,154 @@ function NewProductModal({ open, handleOpen, setAllProducts }) {
             {alert && <Alert message={alert.message} type={alert.type} onClose={() => setAlert(null)} />}
             <Dialog open={open} handler={handleOpen}>
                 <DialogHeader onClick={handleOpen}>
-                    Add New Product
+                    <Typography variant="h5" color="blue-gray">
+                        Add New Product
+                    </Typography>
+                    <IconButton
+                        size="sm"
+                        variant="text"
+                        className="!absolute right-3.5 top-3.5"
+                        onClick={handleOpen}
+                    >
+                        <XMarkIcon className="h-4 w-4 stroke-2" />
+                    </IconButton>
                 </DialogHeader>
                 <DialogBody>
-                    <form onSubmit={handleSubmit} className="create-post-form">
-                        <Input
-                            type="text"
-                            label="Brand Name"
-                            name="brand"
-                            value={newProduct.brand}
-                            onChange={handleChange}
-                        />
-                        <Input
-                            type="text"
-                            label="Product Name"
-                            name="name"
-                            value={newProduct.name}
-                            onChange={handleChange}
-                        />
-                        <Input
-                            type="number"
-                            label="Price"
-                            name="price"
-                            value={newProduct.price}
-                            onChange={handleChange}
-                        />
-                        <Input
-                            type="text"
-                            label="Description"
-                            name="description"
-                            value={newProduct.description}
-                            onChange={handleChange}
-                        />
-                        <Select
-                            label="Category"
-                            value={category}
-                            onChange={(value) => {
-                                setCategory(value);
-                                setNewProduct({ ...newProduct, category: value });
-                            }}
-                        >
-                            <Option value="clothes">Clothes</Option>
-                            <Option value="cosmetics">Cosmetics</Option>
-                            <Option value="food and beverage">Food and Beverage</Option>
-                            <Option value="second hand">Second Hand</Option>
-                        </Select>
-                        <Input
-                            type="file"
-                            label="Product Image"
-                            onChange={handleFileChange}
-                        />
-                        <Input
-                            type="number"
-                            label="Quantity"
-                            name="quantity"
-                            value={newProduct.quantity}
-                            onChange={handleChange}
-                        />
-                        {category === "clothes" || category === "second hand" ? (
+                    <form className="create-post-form">
+                        {step === 1 && (
                             <>
                                 <Input
                                     type="text"
-                                    label="Color"
-                                    name="color"
-                                    value={newProduct.color}
+                                    label="Brand Name"
+                                    name="brand"
+                                    value={newProduct.brand}
                                     onChange={handleChange}
                                 />
-                                <div className="my-4">
+                                <Select
+                                    label="Category"
+                                    value={category}
+                                    onChange={(value) => {
+                                        setCategory(value);
+                                        setNewProduct({ ...newProduct, category: value });
+                                    }}
+                                >
+                                    <Option value="clothes">Clothes</Option>
+                                    <Option value="cosmetics">Cosmetics</Option>
+                                    <Option value="food and beverage">Food and Beverage</Option>
+                                    <Option value="second hand">Second Hand</Option>
+                                </Select>
+                                <Input
+                                    type="text"
+                                    label="Product Name"
+                                    name="name"
+                                    value={newProduct.name}
+                                    onChange={handleChange}
+                                />
+                            </>
+                        )}
+                        {step === 2 && (
+                            <>
+                                <Input
+                                    type="file"
+                                    label="Product Image"
+                                    onChange={handleFileChange}
+                                />
+                                <Input
+                                    type="text"
+                                    label="Description"
+                                    name="description"
+                                    value={newProduct.description}
+                                    onChange={handleChange}
+                                />
+                                <Input
+                                    type="number"
+                                    label="Price"
+                                    name="price"
+                                    value={newProduct.price}
+                                    onChange={handleChange}
+                                />
+                                <Input
+                                    type="number"
+                                    label="Quantity"
+                                    name="quantity"
+                                    value={newProduct.quantity}
+                                    onChange={handleChange}
+                                />
+                                {category === "clothes" || category === "second hand" ? (
+                                    <>
+                                        <Input
+                                            type="text"
+                                            label="Color"
+                                            name="color"
+                                            value={newProduct.color}
+                                            onChange={handleChange}
+                                        />
+                                        <div className="my-4">
+                                            <Input
+                                                type="text"
+                                                label="Add Size"
+                                                value={newSize}
+                                                onChange={(e) => setNewSize(e.target.value)}
+                                            />
+                                            <Button color="blue" onClick={handleAddSize}>
+                                                Add Size
+                                            </Button>
+                                        </div>
+                                        {sizes.length > 0 && (
+                                            <div className="my-4">
+                                                <h4>Sizes:</h4>
+                                                <ul className="list-disc ml-4">
+                                                    {sizes.map((size, index) => (
+                                                        <li key={index} className="flex justify-between">
+                                                            {size}
+                                                            <Button color="red" onClick={() => handleRemoveSize(size)}>
+                                                                Remove
+                                                            </Button>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        )}
+                                    </>
+                                ) : null}
+                                {category === "cosmetics" ? (
                                     <Input
                                         type="text"
-                                        label="Add Size"
-                                        value={newSize}
-                                        onChange={(e) => setNewSize(e.target.value)}
+                                        label="Ingredients"
+                                        name="ingredients"
+                                        value={newProduct.ingredients || ""}
+                                        onChange={handleChange}
                                     />
-                                    <Button color="blue" onClick={handleAddSize}>
-                                        Add Size
-                                    </Button>
-                                </div>
-                                {sizes.length > 0 && (
-                                    <div className="my-4">
-                                        <h4>Sizes:</h4>
-                                        <ul className="list-disc ml-4">
-                                            {sizes.map((size, index) => (
-                                                <li key={index} className="flex justify-between">
-                                                    {size}
-                                                    <Button color="red" onClick={() => handleRemoveSize(size)}>
-                                                        Remove
-                                                    </Button>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                )}
+                                ) : null}
+                                {category === "food and beverage" ? (
+                                    <Input
+                                        type="text"
+                                        label="Nutrition Facts"
+                                        name="nutritionFacts"
+                                        value={newProduct.nutritionFacts || ""}
+                                        onChange={handleChange}
+                                    />
+                                ) : null}
                             </>
-                        ) : null}
-                        {category === "cosmetics" ? (
-                            <Input
-                                type="text"
-                                label="Ingredients"
-                                name="ingredients"
-                                value={newProduct.ingredients || ""}
-                                onChange={handleChange}
-                            />
-                        ) : null}
-                        {category === "food and beverage" ? (
-                            <Input
-                                type="text"
-                                label="Nutrition Facts"
-                                name="nutritionFacts"
-                                value={newProduct.nutritionFacts || ""}
-                                onChange={handleChange}
-                            />
-                        ) : null}
+                        )}
+                        <div className="flex justify-between mt-4">
+                            {step > 1 && (
+                                <Button color="blue" type="button" onClick={() => setStep(step - 1)}>
+                                    Previous
+                                </Button>
+                            )}
+                            {step < 2 ? (
+                                <Button color="green" type="button" onClick={() => setStep(step + 1)}>
+                                    Next
+                                </Button>
+                            ) : (
+                                <Button color="green" type="button" onClick={handleSubmit}>
+                                    Save Product
+                                </Button>
+                            )}
+                        </div>
                     </form>
                 </DialogBody>
-                <DialogFooter>
-                    <Button color="green" type="submit" onClick={handleSubmit}>
-                        Save Product
-                    </Button>
-                </DialogFooter>
             </Dialog>
         </>
     );
