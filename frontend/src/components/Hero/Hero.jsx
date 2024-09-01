7d49c19468ec105c1b37e8e5f0de1e501fce1bf8
Luca-Gerace <geraceluca91@gmail.com>
2024-09-02 00:34:48 +0200

    FRONTEND: home

---
+++ frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import './Hero.css'
+import { Button } from "@material-tailwind/react";
+
+export default function Hero() {
+    return (
+        <div className='hero w-full h-[250px] md:h-[350px] -mt-8 relative'>
+            <div className='flex flex-col gap-3 absolute right-6 top-6 md:right-32 md:top-16 w-64 md:w-80'>
+                <h2 className='text-[26px] md:text-[36px] text-white font-bold leading-8 md:leading-10'>Le <span className='text-green-500'>migliori offerte</span> scelte per te</h2>
+                <p className='text-[20px] md:text-[24px] text-white'>Capi d’abbigliamento in saldo fino al 50%</p>
+                <Button className='bg-green-500 rounded-full py-4'>Vedi prodotti</Button>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,16 @@
+import Hero from "../components/Hero/Hero"
+import CategoryCards from "../components/ImageCard/CategoryCards"
+
 export default function Home() {
+
+
   return (
-    <div>Home</div>
+    <>
+      <Hero />
+      <div className="flex flex-col gap-4 py-8 px-4 lg:px-0">
+        <h1 className="pb-4 text-[32px] md:text-[38px] text-black font-bold leading-10 md:leading-[50px]">Il <span className='text-green-500'>marketplace</span> che sostiene la tua scelta di <span className='text-green-500'>vita sostenibile</span></h1>
+        <CategoryCards />
+      </div>
+    </>
   )
 }
